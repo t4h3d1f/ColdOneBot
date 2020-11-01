9e27e168160392a06e3bd0a81547fc2477f0c769
Toomas Allen <thoomasallen8@gmail.com>
2020-11-01 09:23:18 -0800

    added new automeme feature

--- application.py
+++ application.py
@@ -9,28 +9,39 @@ from discord.ext import commands
 import pymysql.cursors
 from random import randint
 from bisect import bisect_right
-
-
-
-#        if message.content.startswith('crack open a cold one'):
-#            sql = "INSERT INTO coldOnes (time, username) VALUES (%s,%s)"
-#            vals = (message.created_at, message.author.name)
-#            mycursor.execute(sql,vals)
-#            mydb.commit()
-#            mycursor.execute("SELECT COUNT(*) FROM coldOnes")
-#            myresult = mycursor.fetchall()
-#            await message.channel.send("Nice! {0} cold ones have been opened!".format(myresult[0][0]))
-
+import threading
 
 bot = commands.Bot(command_prefix="&")
 
 # probability a track will play (1-1000) and the track name
-
 tracks = [
     (750, 'yooo.m4a'),
     (990, 'YOOOOOOOOOOOUUUUUUU (152kbit_AAC).m4a'),
     (1000, 'Discord Message Sounds.m4a')
 ]
+
+memeTracks = [
+    'Discord Message Sounds.m4a',
+    'A True Meme War Veteran.m4a',
+    'accidentally start a endgame boss fight.m4a',
+    'Are you sober now.m4a',
+    'beating my damn dick.m4a',
+    'Bruh.m4a',
+    'CHOCOLATE.m4a',
+    'Do I look like I know what a JPEG is.m4a',
+    'GameCube intro.m4a',
+    'hardcore hacking mode.m4a',
+    'insert drive.m4a',
+    'Morrowind.m4a',
+    'Omae Wa Mou Shindeiru.m4a',
+    'Poisoned.m4a',
+    'Potion Seller.m4a',
+    'Tell Me Does He Look Like a BITCH.m4a',
+    'What is the airspeed velocity of an unladen swallow.m4a'
+]
+# store meme thread
+memeThread = None
+memer = None
 
 
 def getConnection():
@@ -95,13 +106,34 @@ async def coco(ctx):
     # Generate a number between 1 and 1000
     audiochance = randint(1, 1000)
     # figure out what track this corrisponds to
-    index = bisect_right(tracks, (audiochance,''))
+    index = bisect_right(tracks, (audiochance, ''))
     source = FFmpegPCMAudio(tracks[index][1])
     ctx.voice_client.play(source)
     while ctx.voice_client.is_playing():
         await asyncio.sleep(1)
     await ctx.voice_client.disconnect()
     mydb.close()
+
+
+# Start monitoring for user in voice channel.
+# once enabled will join voice chat after a random amount of time
+# and play a meme audio clip.
+# continues until manually disabled with "nofunnybusiness" or
+# user leaves voice chat.
+@bot.command(name="automeme", help="Automatic meming (☞⌐▀͡ ͜ʖ͡▀ )☞")
+async def automeme_enable(message):
+    memer = message.author
+    memeThread = threading.Timer(randint(90, 360), blast_meme)
+    memeThread.start()
+
+
+# Stop memeThread and clear original user from memory
+@bot.command(name="nofunnybusiness", help="Disables automeme")
+async def automeme_disable(message):
+    if memeThread is not None:
+        if memeThread.is_alive():
+            memeThread.cancel()
+            memer = None
 
 
 @bot.command(name="stats", help="View your drinking stats")
@@ -117,6 +149,24 @@ async def stats(message):
     if result[0][0] > 100:
         await message.channel.send('you alcholholic')
     mydb.close()
+
+
+def blast_meme():
+    channel = bot.get_channel((bot.get_user(memer.id)).voice.channel.id)
+    if not channel:
+        memeThread.cancel()
+        return
+    if memer not in channel.members:
+        memeThread.cancel()
+        return
+    await channel.connect()
+    # Pick a random track
+    idx = randint(0, len(memeTracks)-1)
+    source = FFmpegPCMAudio(memeTracks[idx])
+    ctx.voice_client.play(source)
+    while ctx.voice_client.is_playing():
+        await asyncio.sleep(1)
+    await ctx.voice_client.disconnect()
 
 
 @bot.command(name="leaderboard",
