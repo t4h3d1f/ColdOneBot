eda95a1c373a3cbb54701d22f0fde5106d3790f9
Sean Leisle <sleisle@gmail.com>
2021-01-03 16:07:58 -0800

    Voting working'

--- Bet.py
+++ Bet.py
@@ -1,10 +1,11 @@
 import discord
-from Discord.HasEmbed import HasEmbed
 from ColdOneCore import CoreColors
+from VoteBase import VoteBase
 
 pogUrl = "https://img2.123clipartpng.com/poggers-transparent-picture-2101472-poggers-transparent-poggers-emote-transparent-clipart-300_300.png"
+defaultAmount = 50
 
-class Bet(HasEmbed):
+class Bet(VoteBase):
 
     activeBets = {}
 
@@ -20,47 +21,131 @@ class Bet(HasEmbed):
         del Bet.activeBets[author]
         return bet
 
+    # Verifies that each user involved in the bet exists in the user table
+    @staticmethod
+    def checkUsersExist(payout, myCursor, db):
+        usersToCheck = payout['winners'] + payout['losers']
+        usersToAdd = []
+        getAllSql = "SELECT discord_user_id FROM pogs;"
+        insertSql = "INSERT INTO pogs(discord_user_id, username, pogs) VALUES (%s, %s, %s);"
+        myCursor.execute(getAllSql)
+        getAllRet = myCursor.fetchall()
+        print(str(getAllRet))
+        for curUser in usersToCheck:
+            tuple = (str(curUser.id),)
+            if not tuple in getAllRet:
+                # add user
+                insertVals = (str(curUser.id), str(curUser.name), 1000)
+                myCursor.execute(insertSql, insertVals)
+                db.commit()
+
+    # Updates pog table with winner and loser amounts
+    @staticmethod
+    def updateUserPogs(payout, myCursor, db):
+        if ((not payout['winners']) or (not payout['losers'])):
+            return
+
+        sqlUpdateQuery = "";
+        for curWinner in payout['winners']:
+            sqlUpdateQuery += "UPDATE pogs SET pogs = pogs + " + str(payout['winAmount'])
+            sqlUpdateQuery += " WHERE discord_user_id = " + str(curWinner.id) + "\n"
+        for curLoser in payout['losers']:
+            sqlUpdateQuery += "UPDATE pogs SET pogs = pogs - " + str(payout['loseAmount'])
+            sqlUpdateQuery += " WHERE discord_user_id = " + str(curLoser.id) + "\n"
+        sqlUpdateQuery += ";"
+        if sqlUpdateQuery:
+            myCursor.execute(sqlUpdateQuery)
+            db.commit()
+
+    # Returns all rows from pogs
+    @staticmethod
+    def selectAllPogs():
+        db = getConnection()
+        myCursor = db.cursor()
+        sql = "SELECT * FROM pogs ORDER BY pogs DESC;"
+        myCursor.execute(sql)
+        return myCursor.fetchall()
+
+    # Prints out all pogs to python output
+    @staticmethod
+    def selectAndPrintAll():
+        print("All pogs: ")
+        rows = Bet.selectAllPogs()
+        for row in rows:
+            print(f'id:{row[0]} disc_id:{row[1]} user:{row[2]} pogs:{row[3]}')
+
+    # Closes down a bet
+    #
+    # TODO: Edit or delete old embed from bet creation
+    @staticmethod
+    async def closeBet(db, bet, message):
+        if not bet:
+            return
+        forsWin = message.find("win") != -1
+        payouts = await bet.calculatePayouts(forsWin)
+        print("Payout: " + str(payouts))
+        myCursor = db.cursor()
+        Bet.checkUsersExist(payouts, myCursor, db)
+        Bet.updateUserPogs(payouts, myCursor, db)
+        await bet.getCloseEmbed(payouts)
+        return
+
+    # Sets up a bet, format being &bet
+    # Ex: &bet I'm gonna beat your ass in TFT
+    # Ex: &bet 25, You'll laugh before me
+    @staticmethod
+    async def createBet(ctx, author, message):
+        amount = defaultAmount
+        description = "";
+        msgSplit = message.split(", ")
+        if len(msgSplit) > 1:
+            amount = int(msgSplit[0].strip())
+            description = msgSplit[1]
+        else:
+            description = msgSplit[0]
+        bet = Bet(author, amount, description)
+        await bet.sendEmbed(ctx)
+
     # Constructor, checks if the author has a bet and if not, adds to bets
     def __init__(self, author, amount = 50, desc = ""):
+        # Only one bet per user is allowed
         if Bet.doesUserBetExist(author):
             return null
         self.amount = amount
         self.author = author
-
-        self.embedTitle = "Poggy bet"
-        self.embedAuthor = self.author
-        self.embedDescription = "The bet amount is " + str(self.amount) + " pogs."
+        self.betDesc = desc
+        title = "Poggy bet"
+        embedDescription = "The bet amount is " + str(self.amount) + " pogs."
         if len(desc) > 0:
-            self.embedTitle += ": " + desc
-        self.embedFooter = "Bet with them with +1, or against them with -1."
-        self.embedFooter += " End the bet by typing &bet, followed by win or lose."
+            title += ": " + desc
+        footer = "Bet with them with +1, or against them with -1. End the bet by typing &bet, followed by win or lose."
+        super().__init__(
+            title=title,
+            author=self.author,
+            desc=embedDescription,
+            footer=footer,
+            color=CoreColors.InteractColor,
+            thumbnail=pogUrl)
         Bet.activeBets.update({self.author:self})
-
-    # Creates the embed object for this bet
-    def getEmbed(self):
-        self.embed = discord.Embed(color=CoreColors.InteractColor, title=self.embedTitle, description=self.embedDescription)
-        self.embed.set_author(name=self.embedAuthor)
-        self.embed.set_footer(text=self.embedFooter)
-        self.embed.set_thumbnail(url=pogUrl)
-        return self.embed
 
     # Calculates the payouts for the winners and losers
     async def calculatePayouts(self, forsWin = True):
-        voteTally = self.countVote()
+        voteTally = await self.countVote()
+        print(voteTally)
         countWinners = 0
         countLosers = 0
         winUsers = []
         loseUsers = []
         if forsWin:
-            countWinners = len(voteTally['voteFor'])
-            countLosers = len(voteTally['voteAgainst'])
-            winUsers = voteTally['voteFor']
-            loseUsers = voteTally['voteAgainst']
+            countWinners = len(voteTally.get('voteFor'))
+            countLosers = len(voteTally.get('voteAgainst'))
+            winUsers = voteTally.get('voteFor')
+            loseUsers = voteTally.get('voteAgainst')
         else:
-            countWinners = len(voteTally['voteAgainst']);
-            countLosers = len(voteTally['voteFor'])
-            winUsers = voteTally['voteAgainst']
-            loseUsers = voteTally['voteFor']
+            countWinners = len(voteTally.get('voteAgainst'));
+            countLosers = len(voteTally.get('voteFor'))
+            winUsers = voteTally.get('voteAgainst')
+            loseUsers = voteTally.get('voteFor')
 
         # Validate users lists. Anyone who bets twice loses
         for user in loseUsers:
@@ -73,3 +158,11 @@ class Bet(HasEmbed):
         retDict['winners'] = winUsers
         retDict['losers'] = loseUsers
         return retDict
+
+    async def getCloseEmbed(self, payout):
+        self.embed = None
+        self.embedFooter = ""
+        self.embedTitle = "Bet over: " + self.betDesc
+        self.embedDescription = "Winners get " + str(payout.get('winAmount')) + ". "
+        self.embedDescription += "Losers lose " + str(payout.get('loseAmount')) + "."
+        await self.channel.send(embed=self.getEmbed())
--- VoteBase.py
+++ VoteBase.py
@@ -2,13 +2,40 @@ import discord
 
 from Discord.HasEmbed import HasEmbed
 
+# Defines a base object to handle tallying and creating embeds for voting.
 class VoteBase(HasEmbed):
 
-    def __init__(self, title = "", author = "", desc = "", footer = "", )
+    # VoteBase constructor, sets up fields for embed creation
+    def __init__(self, title, author=None, desc=None, footer=None, color=None, thumbnail=None):
+        self.embedTitle = title
+        if author:
+            self.embedAuthor = author
+        if desc:
+            self.embedDescription = desc
+        if footer:
+            self.embedFooter = footer
+        if thumbnail:
+            self.embedThumbnail = thumbnail
+        if color:
+            self.embedColor = color
+
+    # Creates the embed object for this vote base
+    def getEmbed(self):
+        if hasattr(self, 'embedDescription'):
+            self.embed = discord.Embed(color=self.embedColor, title=self.embedTitle, description=self.embedDescription)
+        else:
+            self.embed = discord.Embed(color=self.embedColor, title=self.embedTitle)
+        if hasattr(self, 'embedAuthor'):
+            self.embed.set_author(name=self.embedAuthor)
+        if hasattr(self, 'embedFooter'):
+            self.embed.set_footer(text=self.embedFooter)
+        if hasattr(self, 'embedThumbnail'):
+            self.embed.set_thumbnail(url=self.embedThumbnail)
+        return self.embed
 
     # Sends an embed of this VoteBase to the given context.
     # Returns the discord.Message object from the embed send.
-    def sendEmbed(self, ctx):
+    async def sendEmbed(self, ctx):
         self.channel = ctx.channel
         sentEmbedMsg = await ctx.channel.send(embed=self.getEmbed())
         # Add reacts
@@ -18,7 +45,9 @@ class VoteBase(HasEmbed):
         self.embedMsgId = sentEmbedMsg.id
         return sentEmbedMsg
 
-    def countVote(self):
+    # Ends the vote and counts up users' reactions.
+    # Returns a dictionary of the users who voted for and against.
+    async def countVote(self):
         retDict = {}
         # Count of the reactions
         usersFor = []
@@ -36,4 +65,14 @@ class VoteBase(HasEmbed):
                         usersAgainst.append(user)
         retDict['voteFor'] = usersFor
         retDict['voteAgainst'] = usersAgainst
+        await msg.delete()
         return retDict
+
+    # Removes the embed
+    # TODO: This throws an exception sometimes, don't know why
+    async def clearEmbed(self):
+        try:
+            msg = await self.channel.fetch_message(self.embedMsgId)
+            await msg.delete()
+        except:
+            print("Exception from deleting message")
