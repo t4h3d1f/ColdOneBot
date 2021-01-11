a14341bd3da8d1f1f6fb737bf63b109b62d15dbf
nvitha <nick.vitha.ctr@sage.northcom.mil>
2021-01-10 21:56:29 -0800

    Revert "Initial commit to add rolling a die"
    This reverts commit 0298f638faa5a71ad2f11e8b8f4f63e2ef0b0a24.

--- application.py
+++ application.py
@@ -18,7 +18,6 @@ from Vote import Vote
 from JokeGetter import JokeGetter
 from Vote import Vote
 from EventHandlers.ReactionHandler import ReactionHandler
-from Roll import Roll
 
 botPrefix = "&"
 bot = commands.Bot(command_prefix=botPrefix)
@@ -301,12 +300,6 @@ async def fixMoney(ctx):
     mycursor = db.cursor()
     mycursor.execute("UPDATE pogs SET pogs = 1000")
     db.commit()
-
-@bot.command(name="roll", help="Roll a die (&roll d<1-1000>")
-async def rollDie(ctx):
-    cmdBase = botPrefix + "roll "
-    message = ctx.message.content[len(cmdBase):]
-    ctx.message.channel.send(await Roll.roll(message))
 
 @bot.event
 async def on_reaction_add(reaction, user):
